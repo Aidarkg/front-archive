c60a72436264ea8ca2e4d8f0095250a18410dacc
Aiat <138316674+BAayah@users.noreply.github.com>
2024-04-26 22:34:14 +0600

    added banner

--- src/pages/main/section/banner/Banner.jsx
+++ src/pages/main/section/banner/Banner.jsx
@@ -1,3 +1,12 @@
+import img from "./images/hero-bg@2x.png";
+import s from "./Banner.module.sass";
+
 export const Banner = () => {
-   return <div></div>;
+   return (
+      <section className={s.banner}>
+         <div>
+            <img className={s.img} src={img} alt="building"/>
+         </div>
+      </section>
+   );
 };
