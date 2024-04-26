acf476fe4e8a65236840346c5eff71112e825ee1
Azret19 <assassin.200219@gmail.com>
2024-04-26 11:25:50 +0600

    feat(HelpSection.jsx/HelpSection.module.sass/Header.jsx/Header.module.sass): added a HelpSection and changed Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,50 @@
-import { NavbarList } from "../../utils/NavbarList.jsx";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import s from "./Header.module.sass";
+import { NavbarList } from "../../utils/NavbarList";
 
 export const Header = () => {
    return (
-      <section className={s.container}>
-         <header className={s.navbar}>
-            <ul>
-               <li>
+      <header className={s.header}>
+         <div className={s.header__inner}>
+            <span className={s.header__logo}>
+               <h3>АРХИВ ПРЕЗИДЕНТА</h3>
+               <h6>КЫРГЫЗСКОЙ РЕСПУБЛИКИ</h6>
+            </span>
+            <nav>
+               <ul className={s.header__nav_list}>
                   {NavbarList.map(item => (
-                     <Link to={item.route} key={item.id}>
-                        {item.caption}
-                     </Link>
-                  ))}
-               </li>
+                     <li key={item.id}>
+                        <NavLink to={item.route}>
+                           {item.caption}
+                        </NavLink>
+                     </li>
+                  )).slice(0, 5)}
+               </ul>
+            </nav>
+            <ul className={s.header__languages_list}>
+               <li>РУС</li>
+               <li>КЫР</li>
+               <li>ENG</li>
             </ul>
-         </header>
-      </section>
+            <input className={s.header__input} type="search" placeholder="Поиск по сайту" />
+         </div>
+      </header>
    );
 };
+
+
+
+
+{/* <section className={s.container}>
+<header className={s.navbar}>
+   <ul>
+      <li>
+         {NavbarList.map(item => (
+            <Link to={item.route} key={item.id}>
+               {item.caption}
+            </Link>
+         ))}
+      </li>
+   </ul>
+</header>
+</section> */}
