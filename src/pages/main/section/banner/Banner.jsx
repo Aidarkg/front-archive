86b67c00c2444e4e1335d646b86a8ddf6279ce67
Aiat <138316674+BAayah@users.noreply.github.com>
2024-05-07 00:22:22 +0600

    added some images

--- src/pages/main/section/banner/Banner.jsx
+++ src/pages/main/section/banner/Banner.jsx
@@ -1,4 +1,5 @@
 import img from "./images/hero-bg@2x.png";
+import img1 from "./images/symbol.svg";
 import s from "./Banner.module.sass";
 
 export const Banner = () => {
@@ -6,6 +7,13 @@ export const Banner = () => {
       <section className={s.banner}>
          <div>
             <img className={s.img} src={img} alt="building"/>
+            <div className={s.h1img}>
+                <img className={s.img1} src={img1} alt="symbol"/>
+                <h1>АРХИВ ПРЕЗИДЕНТА КЫРГЫЗСКОЙ <br />РЕСПУБЛИКИ</h1>
+            </div>
+            <p>С ДРУГОЙ СТОРОНЫ, СУЩЕСТВУЮЩАЯ ТЕОРИЯ ПОЗВОЛЯЕТ ОЦЕНИТЬ ЗНАЧЕНИЕ 
+               <br/> УКРЕПЛЕНИЯ МОРАЛЬНЫХ ЦЕННОСТЕЙ.ВНЕЗАПНО, ДЕЙСТВИЯ ПРЕДСТАВИТЕЛЕЙ</p>
+            <button className={s.buttonr}>ЗАДАТЬ ВОПРОС</button>            
          </div>
       </section>
    );
