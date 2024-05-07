431e993740d73882652b315b48eb695fe5003c14
Aiat <138316674+BAayah@users.noreply.github.com>
2024-05-07 16:49:59 +0600

    styling banner section

--- src/pages/main/section/banner/Banner.jsx
+++ src/pages/main/section/banner/Banner.jsx
@@ -1,20 +1,17 @@
-import img from "./images/hero-bg@2x.png";
+// import img from "./images/hero-bg@2x.png";
 import img1 from "./images/symbol.svg";
-import s from "./Banner.module.sass";
+import css from "./Banner.module.sass";
 
 export const Banner = () => {
    return (
-      <section className={s.banner}>
-         <div>
-            <img className={s.img} src={img} alt="building"/>
-            <div className={s.h1img}>
-                <img className={s.img1} src={img1} alt="symbol"/>
-                <h1>АРХИВ ПРЕЗИДЕНТА КЫРГЫЗСКОЙ <br />РЕСПУБЛИКИ</h1>
-            </div>
-            <p>С ДРУГОЙ СТОРОНЫ, СУЩЕСТВУЮЩАЯ ТЕОРИЯ ПОЗВОЛЯЕТ ОЦЕНИТЬ ЗНАЧЕНИЕ 
-               <br/> УКРЕПЛЕНИЯ МОРАЛЬНЫХ ЦЕННОСТЕЙ.ВНЕЗАПНО, ДЕЙСТВИЯ ПРЕДСТАВИТЕЛЕЙ</p>
-            <button className={s.buttonr}>ЗАДАТЬ ВОПРОС</button>            
+      <section className={css.banner}>         
+         <div className={css.main}>                        
+             <img className={css.img1} src={img1} alt="symbol"/>
+             <h1 className={css.h1}>АРХИВ ПРЕЗИДЕНТА КЫРГЫЗСКОЙ <br />РЕСПУБЛИКИ</h1>            
+             <p className={css.p}>С ДРУГОЙ СТОРОНЫ, СУЩЕСТВУЮЩАЯ ТЕОРИЯ ПОЗВОЛЯЕТ ОЦЕНИТЬ ЗНАЧЕНИЕ 
+               <br/> УКРЕПЛЕНИЯ МОРАЛЬНЫХ ЦЕННОСТЕЙ.ВНЕЗАПНО, ДЕЙСТВИЯ ПРЕДСТАВИТЕЛЕЙ</p>                      
          </div>
+         <button className={css.button}>ЗАДАТЬ ВОПРОС</button>  
       </section>
    );
 };
