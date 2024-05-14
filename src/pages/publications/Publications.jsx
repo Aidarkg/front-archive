084dfa9c9b2696110f4bc9d3e6441d8762d13a37
ayanaibraeva <ayanafront@gmail.com>
2024-05-14 17:41:20 +0600

    add folders

---
+++ src/pages/publications/Publications.jsx
@@ -0,0 +1,7 @@
+export const Publications = () => {
+    return(
+        <div>
+            <h1>Publications</h1>
+        </div>
+    );
+};
---
+++ src/pages/video/Video.jsx
@@ -0,0 +1,7 @@
+export const Video = () => {
+    return (
+        <div>
+            <h1>Video</h1>
+        </div>
+    );
+};
