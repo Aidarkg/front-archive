f5852422ad2b2c03aa02b53cbf91ec8f08430f89
Azret19 <assassin.200219@gmail.com>
2024-05-17 19:38:22 +0600

    feat(Breadcrumbs): added a breadcrumbs

---
+++ src/ui/breadcrumbs/Breadcrumbs.jsx
@@ -0,0 +1,72 @@
+import styles from "./Breadcrumbs.module.sass";
+
+import { NavLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
+import { Typography } from "../../Typography/Typography";
+
+import { ArrowRightSvg } from "../Svg/ArrowRightSvg";
+
+
+export const Breadcrumbs = ({ currentPage, parentPageLink, currentPageId }) => {
+
+    const { t } = useTranslation();
+
+    const crumbs = [];
+
+    crumbs.push(
+        <li key="main">
+            <NavLink to={"/"}>
+                <Typography variant="h6" color="blue300" >
+                    {t("main")}
+                </Typography>
+            </NavLink>
+        </li>
+    );
+
+    crumbs.push(
+        <li key="icon1">
+            <ArrowRightSvg />
+        </li>
+    );
+
+    if (currentPage && parentPageLink) {
+        crumbs.push(
+            <li key="currentLink">
+                <NavLink to={parentPageLink}>
+                    <Typography className={styles.breadCrumbTitle} variant="h6" color="blue300" >
+                        {currentPage}
+                    </Typography>
+                </NavLink>
+            </li>
+        );
+
+        crumbs.push(
+            <li key="icon2">
+                <ArrowRightSvg />
+            </li>
+        );
+
+        crumbs.push(
+            <li key="currentTitle">
+                <Typography className={styles.breadCrumbTitle} variant="h6" color="blue500" >
+                    {currentPageId}
+                </Typography>
+            </li>
+        );
+
+    } else if (currentPage) {
+        crumbs.push(
+            <li key="currentTitle">
+                <Typography className={styles.breadCrumbTitle} variant="h6" color="blue500" >
+                    {currentPage}
+                </Typography>
+            </li>
+        );
+    }
+
+    return <ul className={styles.breadCrumbList}>{crumbs}</ul>;
+
+};
+
+
