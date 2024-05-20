c535b1e7f4edf2cb4a9f7d83c26eecc5e481160f
ayanaibraeva <ayanafront@gmail.com>
2024-05-20 16:51:57 +0600

    fixed errors related to eslint

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -2,10 +2,8 @@ import { Banner } from "../../components/banner/Banner.jsx";
 import { faqList } from "../FAQ/scripts/AccardionMenu.jsx";
 import personFaq from "../../pages/FAQ/img/personFaq.svg";
 import { Accardion } from "../../ui/Accordion/Accordion.jsx";
-<<<<<<< src/pages/main/Main.jsx
 import {MainGallery} from "./mainGallery/MainGallery.jsx";
 import { AboutArchive } from "../aboutArchive/AboutArchive.jsx";
-import { MainGallery } from "./mainGallery/MainGallery.jsx";
 import { Organizations } from "./organizations/Organizations.jsx";
 import ContactSection from "../../components/contactSection/ContactSection.jsx";
 
