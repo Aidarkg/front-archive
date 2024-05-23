9e7e727e4e87a8c3ddce4e6ca4572e8c08c721d3
ayanaibraeva <ayanafront@gmail.com>
2024-05-23 14:11:39 +0600

    adjusted the modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import LayoutRoutes from "./app/layoutRoutes/LayoutRoutes.jsx";
 import { Header } from "./components/header/Header.jsx";
-import {ModalPage} from "./shared/modalPage/ModalPage.jsx";
-
 
 function App() {
    return (
-      <div>
+      <>
          <Header />
-          <ModalPage/>
          <Routes>
             <Route path="*" element={<LayoutRoutes />} />
          </Routes>
-      </div>
+      </>
    );
 }
 
--- src/pages/FAQ/Faq.jsx
+++ src/pages/FAQ/Faq.jsx
@@ -3,11 +3,13 @@ import { Accardion } from "../../ui/Accordion/Accordion";
 import { faqList } from "./scripts/AccardionMenu";
 import classes from "./Faq.module.sass";
 import {Typography} from "../../Typography/Typography.jsx";
+import {Breadcrumbs} from "../../ui/breadcrumbs/Breadcrumbs.jsx";
 
 export const Faq = () => {
    return (
       <section>
         <div className="container">
+            <Breadcrumbs currentPage={"вопросы и ответы"} />
             <div className={classes.faq}>
                 <div>
                     <Accardion faqList={faqList} />
