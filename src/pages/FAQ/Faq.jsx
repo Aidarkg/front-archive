6ae64c1c3244a621345116321e03d2287c5971be
ayanaibraeva <ayanafront@gmail.com>
2024-05-26 15:47:07 +0600

    Added a common port, fixed fonts

--- src/pages/FAQ/Faq.jsx
+++ src/pages/FAQ/Faq.jsx
@@ -5,26 +5,29 @@ import classes from "./Faq.module.sass";
 import { Typography } from "../../Typography/Typography.jsx";
 import { Breadcrumbs } from "../../modules/breadcrumbs/Breadcrumbs.jsx";
 import { t } from "i18next";
+import {Container} from "../../components/container/Container.jsx";
 
 export const Faq = () => {
    return (
       <section>
-         <Breadcrumbs currentPage={"вопросы и ответы"} />
-         <div className={classes.faq}>
-            <div>
-               <Accardion faqList={faqList} />
-            </div>
-            <div>
-               <Typography
-                  className={classes.heading}
-                  variant="h1"
-                  color="blue500"
-               >
-                  {t("faq")}
-               </Typography>
-               <QuestionForm />
-            </div>
-         </div>
+          <Container>
+              <Breadcrumbs currentPage={"вопросы и ответы"} />
+              <div className={classes.faq}>
+                  <div>
+                      <Accardion faqList={faqList} />
+                  </div>
+                  <div>
+                      <Typography
+                          className={classes.heading}
+                          variant="h1"
+                          color="blue500"
+                      >
+                          {t("faq")}
+                      </Typography>
+                      <QuestionForm />
+                  </div>
+              </div>
+          </Container>
       </section>
    );
 };
