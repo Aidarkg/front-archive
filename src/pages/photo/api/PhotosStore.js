45f98a4e76154e4ec611cb1a536302cb015e9dce
Aidana <aidanasoww@icloud.com>
2024-05-28 16:25:27 +0600

    add pagination and fixed loader

--- src/pages/photo/api/PhotosStore.js
+++ src/pages/photo/api/PhotosStore.js
@@ -1,4 +1,4 @@
-import { create } from "zustand";
+import {create} from "zustand";
 import axios from "axios";
 
 const BASE_URL = "https://aidarzh.pythonanywhere.com/api/v1";
@@ -11,7 +11,7 @@ export const usePhotos = create((set) => ({
     title: '',
     loading: false,
     getPhotosContent: async () => {
-        set({ loading: true });
+        set({loading: true});
         try {
             const response = await axios.get(`${BASE_URL}/photos`);
             const data = response.data;
@@ -21,14 +21,14 @@ export const usePhotos = create((set) => ({
             });
         } catch (error) {
             console.error('Failed fetch error', error);
-            set({ error: error.message });
+            set({error: error.message});
         } finally {
-            set({ loading: false });
+            set({loading: false});
         }
     },
     loadMorePhotosContent: async (nextPage) => {
         if (!nextPage) return;
-        set({ loading: true });
+        set({loading: true});
         try {
             const response = await axios.get(nextPage);
             const data = response.data;
@@ -39,22 +39,22 @@ export const usePhotos = create((set) => ({
             }));
         } catch (error) {
             console.error('Failed fetch error', error);
-            set({ error: error.message });
+            set({error: error.message});
         } finally {
-            set({ loading: false });
+            set({loading: false});
         }
     },
     getImages: async (id) => {
-        set({ loading: true });
+        set({loading: true});
         try {
             const response = await axios.get(`${BASE_URL}/photos/${id}`);
             const data = await response.data;
-            set({ images: data.photo, title: data.title, error: null });
+            set({images: data.photo, title: data.title, error: null});
         } catch (error) {
             console.error(error.message);
-            set({ error: error.message });
+            set({error: error.message});
         } finally {
-            set({ loading: false });
+            set({loading: false});
         }
     }
 }));
