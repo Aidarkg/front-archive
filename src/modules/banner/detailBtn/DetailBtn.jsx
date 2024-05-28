16f8824e8b614deb6c5ad149355250f6ed28475c
Aiat <138316674+BAayah@users.noreply.github.com>
2024-05-28 10:41:39 +0600

    added translations and adaptive of banner is done

--- src/modules/banner/detailBtn/DetailBtn.jsx
+++ src/modules/banner/detailBtn/DetailBtn.jsx
@@ -2,18 +2,21 @@ import { Link } from "react-router-dom";
 import { ArrowRight } from "../../../assets/icons/ArrowRight";
 import { Typography } from "../../../Typography/Typography";
 import classes from "../detailBtn/DetailBtn.module.sass";
+import { useTranslation } from "react-i18next";
 
 export const DetailBtn = () => {
-  return (
-    <section className="detail">
+   const { t } = useTranslation();
+
+   return (
+    <div className="detail">
        <Link to="/aboutArchive">
        <button className={classes.btn}>
           <Typography className={classes.btnDetail} variant="h6" weight="bold">
-             ПОДРОБНЕЕ ОБ АРХИВЕ
+             {t("mainPage.hero.button")}
                  <ArrowRight />
           </Typography>           
        </button>
        </Link>
-    </section>
+    </div>
   );
 };
