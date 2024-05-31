8337f45bd26a02c3af2084e06428210500da5a6b
Aiat <138316674+BAayah@users.noreply.github.com>
2024-05-31 23:26:30 +0600

    did adaptive of About Archive

--- src/pages/aboutArchive/contentBlocks/ContentBlocks.jsx
+++ src/pages/aboutArchive/contentBlocks/ContentBlocks.jsx
@@ -1,60 +1,4 @@
-// import classes from "./ContentBlocks.module.sass";
-//
-// import { useTranslation } from "react-i18next";
-// import { Typography } from "../../../Typography/Typography";
-// import { Container } from "../../../components/container/Container";
-// import { Btn } from "../../main/mainAboutArchive/btn/Btn";
-//
-// export const ContentBlocks = ({ blocks, showTitle = true, showButton = false }) => {
-//    const { t } = useTranslation();
-//
-//    return (
-//       <section className={classes.main}>
-//             {showTitle && (
-//                <Typography  variant="h1" color="blue500">
-//                   {t("header&footer.subnav.aboutArchive")}
-//                </Typography>
-//             )}
-//             <div >
-//                <div>
-//                    {blocks.map((block, index) => (
-//                        <div
-//                            key={index}
-//                            className={`${classes.contentBlock} ${
-//                                index % 2 !== 0 ? classes.left : " "
-//                            }`}
-//                        >
-//                            <div className={classes.contentBlockText}>
-//                                <Typography className={classes.heading} variant="h2">
-//                                    {block.heading}
-//                                </Typography>
-//                                <Typography className={classes.text} variant="p">
-//                                    {block.text}
-//                                </Typography>
-//                                {showButton && index === 0 && (
-//                                    <div className={classes.buttonContainer}>
-//                                        <Btn />
-//                                    </div>
-//                                )}
-//                            </div>
-//                            <div className={index % 2 !== 0 ? "left" : "right"}>
-//                                <img
-//                                    className={
-//                                        index % 2 !== 0
-//                                            ? classes.leftImage
-//                                            : classes.image
-//                                    }
-//                                    src={block.image}
-//                                    alt="Image"
-//                                />
-//                            </div>
-//                        </div>
-//                    ))}
-//                </div>
-//             </div>
-//       </section>
-//    );
-// };
+
 import { useTranslation } from "react-i18next";
 import { Typography } from "../../../Typography/Typography";
 import { Btn } from "../../main/mainAboutArchive/btn/Btn";
@@ -77,9 +21,20 @@ export const ContentBlocks = ({ blocks, showTitle = true, showButton = false }) 
                         <div
                             key={index}
                             className={`${classes.contentBlock} ${
-                                index % 2 !== 0 ? classes.left : " "
+                                index % 2 === 0 ? classes.left : " "
                             }`}
                         >
+                            <div className={index % 2 !== 0 ? "right" : "left"}>
+                                <img
+                                    className={
+                                        index % 2 !== 0
+                                            ? classes.leftImage
+                                            : classes.image
+                                    }
+                                    src={block.image}
+                                    alt="Image"
+                                />
+                            </div>
                             <div className={classes.contentBlockText}>
                                 <Typography className={classes.heading} variant="h2">
                                     {block.heading}
@@ -92,18 +47,7 @@ export const ContentBlocks = ({ blocks, showTitle = true, showButton = false }) 
                                         <Btn />
                                     </div>
                                 )}
-                            </div>
-                            <div className={index % 2 !== 0 ? "left" : "right"}>
-                                <img
-                                    className={
-                                        index % 2 !== 0
-                                            ? classes.leftImage
-                                            : classes.image
-                                    }
-                                    src={block.image}
-                                    alt="Image"
-                                />
-                            </div>
+                            </div>                            
                         </div>
                     ))}
                 </div>
