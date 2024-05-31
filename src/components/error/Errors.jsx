adcbd482c82bd375e40913421ace0b5b3d463c64
ayanaibraeva <ayanafront@gmail.com>
2024-05-31 13:29:37 +0600

    page 404

--- src/components/error/Errors.jsx
+++ src/components/error/Errors.jsx
@@ -1,9 +1,29 @@
 import notFoundImage from '../../assets/images/404 Page Not Found 1 (1) 1.png';
 import classes from "./Errors.module.sass";
+import {Container} from "../container/Container.jsx";
+import {Typography} from "../../Typography/Typography.jsx";
+import {CustomButton} from "../../UI/customButton/CustomButton.jsx";
 export const Errors = () => {
     return (
-        <div>
-            <img className={classes.img} src={notFoundImage} alt="404 Page Not Found"/>
-        </div>
+        <section>
+            <Container>
+                <div className={classes.error}>
+                    <div>
+                        <img className={classes.errorIMG} src={notFoundImage} alt="404 Page Not Found"/>
+                    </div>
+                    <Typography variant="h1" color="blue500">
+                        Страница не найдена
+                    </Typography>
+                    <div>
+                        <CustomButton>
+
+                        </CustomButton>
+                        <CustomButton>
+
+                        </CustomButton>
+                    </div>
+                </div>
+            </Container>
+        </section>
     );
 };
