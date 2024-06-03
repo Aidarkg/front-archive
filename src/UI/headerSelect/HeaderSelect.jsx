b4c19ca3101003eb8b6e08f50c1030c39cffb2d0
Azret19 <assassin.200219@gmail.com>
2024-06-03 21:33:38 +0600

    feat(Npa): added responsive to Npa. Fixed and decomposed some components and files

--- src/UI/headerSelect/HeaderSelect.jsx
+++ src/UI/headerSelect/HeaderSelect.jsx
@@ -2,18 +2,15 @@ import { useTranslation } from "react-i18next";
 import classes from "./HeaderSelect.module.sass";
 import { useState } from "react";
 import { ArrowDownSvg } from "../Svg/ArrowDownSvg";
-import { useRegulationsStore } from "../../pages/npa/store/useRegulationsStore";
-import {useLanguageStore} from "../../utils/languageStore/UseLanguageStore.js";
+import { useLanguageStore } from "../../utils/languageStore/UseLanguageStore";
 
 export const HeaderSelect = () => {
     const { i18n } = useTranslation();
     const [activeList, setActiveList] = useState(null);
-    const { setLanguage } = useRegulationsStore();
-    const {setChangeLanguage}=useLanguageStore();
+    const { setChangeLanguage } = useLanguageStore();
 
     const changeLanguage = (lng) => {
         i18n.changeLanguage(lng);
-        setLanguage(lng);
         setChangeLanguage(lng);
         setActiveList(null);
     };
--- src/pages/npa/store/useRegulationsStore.js
+++ src/pages/npa/store/useRegulationsStore.js
@@ -1,24 +1,21 @@
 import axios from "axios";
 import { create } from "zustand";
 
-export const useRegulationsStore = create((set, get) => ({
+import { useLanguageStore } from "../../../utils/languageStore/UseLanguageStore";
+
+export const useRegulationsStore = create((set) => ({
     regulationsList: [],
     error: null,
     loading: false,
-    language: "ru",
-
-    setLanguage: (lang) => {
-        set({ language: lang });
-    },
 
     fetchRegulations: async () => {
-        set ({loading: true, error: null});
+        set({ loading: true, error: null });
         try {
-            const { language } = get();
+            const { language } = useLanguageStore.getState();
             const response = await axios.get(`https://aidarzh.pythonanywhere.com/${language}/api/v1/kodeks`);
             set({ regulationsList: response.data.results, loading: false, error: null });
         } catch (error) {
-            set({regulationsList: [], loading: false, error: error.message });
+            set({ regulationsList: [], loading: false, error: error.message });
         }
     },
 }));
