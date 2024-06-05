f0428d8750b0d1b51422199d401522f1ebee5406
Azret19 <assassin.200219@gmail.com>
2024-06-05 11:23:04 +0600

    refactor:(Header, Breadcrumbs): changes in svg style. Some stylistic corrections

--- src/utils/responsiveComponent/ResponsiveComponent.jsx
+++ src/utils/responsiveComponent/ResponsiveComponent.jsx
@@ -16,93 +16,93 @@ export const ResponsiveComponent = ({ type, name, className }) => {
     const [width] = useWindowSize();
     const { t } = useTranslation();
 
-    const renderComponent = () => {
-        switch (type) {
-            case 'headerLogo':
-                if (width <= 420) {
-                    return <HeaderLogoMobile />;
-                } else if (width <= 800) {
-                    return <HeaderLogoTablet />;
-                } else {
-                    return <HeaderLogo />;
-                }
+    const renderComponent = {
+        headerLogo: () => {
+            if (width <= 420) {
+                return <HeaderLogoMobile />;
+            } else if (width <= 800) {
+                return <HeaderLogoTablet />;
+            } else {
+                return <HeaderLogo />;
+            }
+        },
 
-            case 'headerTitle':
-                if (width <= 500) {
-                    return (
-                        <>
-                            <Typography variant="h5" color="grey500">
-                                {t("header&footer.logo.archivePresidentKR")}
-                            </Typography>
-                            <Typography variant="extraSmallBody" color="grey500">
-                                {t("header&footer.logo.kyrgyzRepublic")}
-                            </Typography>
-                        </>
-                    );
-                } else if (width <= 800) {
-                    return (
-                        <>
-                            <Typography variant="h3" color="grey500">
-                                {t("header&footer.logo.archivePresidentKR")}
-                            </Typography>
-                            <Typography variant="smallBody" color="grey500">
-                                {t("header&footer.logo.kyrgyzRepublic")}
-                            </Typography>
-                        </>
-                    );
-                } else {
-                    return (
-                        <>
-                            <Typography variant="h3" color="grey500">
-                                {t("header&footer.logo.archivePresidentKR")}
-                            </Typography>
-                            <Typography variant="body" color="grey500">
-                                {t("header&footer.logo.kyrgyzRepublic")}
-                            </Typography>
-                        </>
-                    );
-                }
+        headerTitle: () => {
+            if (width <= 500) {
+                return (
+                    <>
+                        <Typography variant="h5" color="grey500">
+                            {t("header&footer.logo.archivePresidentKR")}
+                        </Typography>
+                        <Typography variant="extraSmallBody" color="grey500">
+                            {t("header&footer.logo.kyrgyzRepublic")}
+                        </Typography>
+                    </>
+                );
+            } else if (width <= 800) {
+                return (
+                    <>
+                        <Typography variant="h3" color="grey500">
+                            {t("header&footer.logo.archivePresidentKR")}
+                        </Typography>
+                        <Typography variant="smallBody" color="grey500">
+                            {t("header&footer.logo.kyrgyzRepublic")}
+                        </Typography>
+                    </>
+                );
+            } else {
+                return (
+                    <>
+                        <Typography variant="h3" color="grey500">
+                            {t("header&footer.logo.archivePresidentKR")}
+                        </Typography>
+                        <Typography variant="body" color="grey500">
+                            {t("header&footer.logo.kyrgyzRepublic")}
+                        </Typography>
+                    </>
+                );
+            }
+        },
 
-            case 'searchIcon':
-                if (width <= 620) {
-                    return <SearchSvg />;
-                } else if (width <= 850) {
-                    return <SearchTabletSvg />;
-                } else {
-                    return <SearchIcon />;
-                }
+        searchIcon: () => {
+            if (width <= 620) {
+                return <SearchSvg />;
+            } else if (width <= 850) {
+                return <SearchTabletSvg />;
+            } else {
+                return <SearchIcon />;
+            }
+        },
 
-            case 'truncatedRegulations':
-                if (width <= 700) {
-                    return (
-                        <Typography truncate={30} className={className} variant="body" color="black">
-                            {name}
-                        </Typography>
-                    );
-                } else if (width <= 970) {
-                    return (
-                        <Typography truncate={60} className={className} variant="body" color="black">
-                            {name}
-                        </Typography>
-                    );
-                } else if (width <= 1440) {
-                    return (
-                        <Typography truncate={95} className={className} variant="body" color="black">{name}
-                        </Typography>
-                    );
-                } else {
-                    return (
-                        <Typography truncate={55} className={className} variant="body" color="black">
-                            {name}
-                        </Typography>
-                    );
-                }
-
-            default:
-                return null;
+        truncatedRegulations: () => {
+            if (width <= 700) {
+                return (
+                    <Typography truncate={30} className={className} variant="body" color="black">
+                        {name}
+                    </Typography>
+                );
+            } else if (width <= 970) {
+                return (
+                    <Typography truncate={60} className={className} variant="body" color="black">
+                        {name}
+                    </Typography>
+                );
+            } else if (width <= 1440) {
+                return (
+                    <Typography truncate={95} className={className} variant="body" color="black">{name}
+                    </Typography>
+                );
+            } else {
+                return (
+                    <Typography truncate={55} className={className} variant="body" color="black">
+                        {name}
+                    </Typography>
+                );
+            }
         }
+
     };
 
-    return renderComponent();
+    return renderComponent[type] ? renderComponent[type]() : null;
 };
 
