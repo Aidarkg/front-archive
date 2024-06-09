63a1a533d3985033ce7ea0944e88ca40d814947a
Aiat <138316674+BAayah@users.noreply.github.com>
2024-06-10 00:03:57 +0600

    corrected adaptive

--- src/pages/main/mainAboutArchive/MainAboutArchive.jsx
+++ src/pages/main/mainAboutArchive/MainAboutArchive.jsx
@@ -3,32 +3,36 @@ import img from "../../../assets/images/about-pic.png";
 import { useTranslation } from "react-i18next";
 import classes from ".././mainAboutArchive/MainAboutArchive.module.sass";
 
-
 export const MainAboutArchive = () => {
    const { t } = useTranslation();
 
    const blocks = [
-      { 
-         heading: t("aboutArchivePage.section1.heading"), 
-         text: t("aboutArchivePage.section1.description"), 
-         image: img
-       },
-       { 
+      {
+         heading: t("aboutArchivePage.section1.heading"),
+         text: t("aboutArchivePage.section1.description"),
          image: img,
-         heading: t("aboutArchivePage.section2.heading"), 
-         text: t("aboutArchivePage.section2.description")
-       },
-       { 
-         heading: t("aboutArchivePage.section3.heading"), 
-         text: t("aboutArchivePage.section3.description"), 
-         image: img
-       },
+      },
+      {
+         image: img,
+         heading: t("aboutArchivePage.section2.heading"),
+         text: t("aboutArchivePage.section2.description"),
+      },
+      {
+         heading: t("aboutArchivePage.section3.heading"),
+         text: t("aboutArchivePage.section3.description"),
+         image: img,
+      },
    ];
-   
+
    return (
       <section>
          <div className={classes.content}>
-            <ContentBlocks showTitle={false} blocks={blocks.slice(0, 1)} showButton={true} />
+            <ContentBlocks
+               showTitle={false}
+               blocks={blocks.slice(0, 1)}
+               showButton={true}
+               showBreadcrumbs={false}
+            />
          </div>
       </section>
    );
