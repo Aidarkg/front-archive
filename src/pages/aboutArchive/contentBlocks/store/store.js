372d26eec33c415f95ab23a3cf669bee936058b3
ayanaibraeva <ayanafront@gmail.com>
2024-06-12 22:16:53 +0600

    fix error

--- src/pages/aboutArchive/contentBlocks/store/store.js
+++ src/pages/aboutArchive/contentBlocks/store/store.js
@@ -1,25 +1,25 @@
-import axios from "axios";
-import { BASE_URL } from "../../../../utils/constants/Constants";
-import { create } from "zustand";
-
-export const useData = create((set, get) => ({
-    title: [],
-    description: [],
-    photo: [],
-    error: null,
-    loading: false,
-
-    getData: async () => {
-        set({ loading: true, error: null });
-        try {
-            const response = await axios.get(`${BASE_URL}api/v1/about_archive_list`);            
-            set({
-                title: data.title,
-                description: data.description,
-                photo: data.results,
-            });
-        } catch (error) {            
-            set({ error: error.message, loading: false });
-        }
-    }    
-});
+// import axios from "axios";
+// import { BASE_URL } from "../../../../utils/constants/Constants";
+// import { create } from "zustand";
+//
+// export const useData = create((set, get) => ({
+//     title: [],
+//     description: [],
+//     photo: [],
+//     error: null,
+//     loading: false,
+//
+//     getData: async () => {
+//         set({ loading: true, error: null });
+//         try {
+//             const response = await axios.get(`${BASE_URL}api/v1/about_archive_list`);
+//             set({
+//                 title: data.title,
+//                 description: data.description,
+//                 photo: data.results,
+//             });
+//         } catch (error) {
+//             set({ error: error.message, loading: false });
+//         }
+//     }
+// });
