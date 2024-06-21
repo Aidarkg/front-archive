c0db82baf01a3bca68389e8ca8afc9eede5496e0
baiel <noud.mems.qq@gmail.com>
2024-06-21 22:41:24 +0300

    обрезал массив для faq и сделал кнопку для скачивание

--- src/pages/FAQ/store/useFaqStore.jsx
+++ src/pages/FAQ/store/useFaqStore.jsx
@@ -4,10 +4,12 @@ import {BASE_URL} from "../../../utils/constants/Constants.js";
 
 const useFaqStore = create((set) => ({
     faqs: [],
+    slicedFaqs: [],
     fetchFaqs: async() => {
         try {
             const response = await axios.get(`${BASE_URL}api/v1/faq`);
-            set({faqs: response.data.results});
+            set({faqs: response.data});
+            set({ slicedFaqs: response.data.slice(0, 5) });
         } catch (error) {
             console.error("Ошибка при получении данных:", error);
         }
