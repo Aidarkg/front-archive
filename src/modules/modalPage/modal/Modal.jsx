8f0f37f1ae81d3060c4cab0e5fc4fd20376c759c
ayanaibraeva <ayanafront@gmail.com>
2024-06-21 11:04:04 +0600

    Fixed the modal window

--- src/modules/modalPage/modal/Modal.jsx
+++ src/modules/modalPage/modal/Modal.jsx
@@ -2,7 +2,6 @@ import classes from "./Modal.module.sass";
 import {QuestionForm} from "../../../UI/questionForm/QuestionForm.jsx";
 export const Modal = ({ active, setActive }) => {
 
-
     return (
         <div className={`${classes.modal} ${active ? classes.active : ''}`} onClick={() => setActive(false)}>
             <div className={`${classes.modalContent} ${active ? classes.active : ''}`} onClick={e => e.stopPropagation()}>
